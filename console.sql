a0e71438fc223a82ebec3026023647c1f707c175
Sk Fazla Rabby <fazlarabby53@gmail.com>
2021-11-20 15:29:04 +0100

    Update console.sql

--- console.sql
+++ console.sql
@@ -19,9 +19,3 @@ JOIN "Housing" b
     AND a."UniqueID "<>b."UniqueID "
 WHERE a."PropertyAddress" is null;
 
-
---Breaking out address(Address,City,State)
-SELECT
-SUBSTR("PropertyAddress",1,CHARI)
-FROM "Housing"
-
